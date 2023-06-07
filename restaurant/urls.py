f265ee2e52a576628f17d8165f9820e24b979248
sanjeevpraja <sanjeevpraja@gmail.com>
2023-06-07 10:59:51 +0545

    used router register from rest framework

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -1,8 +1,13 @@
 from django.contrib import admin 
-from django.urls import path 
+from django.urls import path, include
 from . import views
-  
+
+
 urlpatterns = [ 
     #path('', views.sayHello, name='sayHello'), 
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('menu/', views.MenuItemsView.as_view()),
+    path('menu/<int:pk>', views.SingleMenuView.as_view()),
 ]
+
+
--- restaurant/views.py
+++ restaurant/views.py
@@ -1,9 +1,29 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from rest_framework.decorators import api_view
+from .models import Menu, Booking
+from .serializers import MenuSerializer, BookingSerializer
+
+from rest_framework import generics, viewsets
+
 # Create your views here.
 def sayHello(request):
     return HttpResponse('hello')
 
 def index(request):
     return render(request, 'index.html', {})
+
+
+# Create your views here. 
+class MenuItemsView(generics.ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+class SingleMenuView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
